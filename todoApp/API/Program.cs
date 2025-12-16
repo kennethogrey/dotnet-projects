5bdf2fc4edbe8738ac190d8a63533767c08e6a4a
kennethogrey <ogirekenneth@gmail.com>
2025-12-17 02:02:38 +0300

    cleaned up Program.cs

--- todoApp/API/Program.cs
+++ todoApp/API/Program.cs
@@ -9,8 +9,6 @@ builder.WebHost.UseKestrel(options =>
     options.AddServerHeader = false;
 });
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 builder.Services.AddOpenTelemetry().ConfigureResource(resource=> resource.AddService("todoApp")).WithTracing(tracing =>
@@ -21,7 +19,6 @@ builder.Services.AddOpenTelemetry().ConfigureResource(resource=> resource.AddSer
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
